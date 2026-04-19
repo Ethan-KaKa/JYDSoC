dbfca639a6c86891b19961132283b6007c7c0bbd
25050157 <1409887338@qq.com>
2026-04-19 16:10:15 +0800

    更新为DRAM为BRAM版

--- dram.sv
+++ dram.sv
@@ -9,6 +9,8 @@ module DRAM(
 import "DPI-C" function void dram_read(input int addr, output int data);
 import "DPI-C" function void dram_write(input int addr, input int data);
 
+/* 异步读，同步写 */
+/*
 always @(*) begin
     dram_read({12'h801, 2'b00, a, 2'b00}, spo);
 end
@@ -18,4 +20,16 @@ always @(posedge clk) begin
         dram_write({12'h801, 2'b00, a, 2'b00}, d);
     end
 end
+*/
+
+/* 同步读，同步写 */
+
+always @(posedge clk) begin
+    dram_read({12'h801, 2'b00, a, 2'b00}, spo);
+    if (we) begin
+        dram_write({12'h801, 2'b00, a, 2'b00}, d);
+    end
+end
+
+
 endmodule
--- irom.sv
+++ irom.sv
@@ -6,7 +6,17 @@ module IROM(
 
 import "DPI-C" function void irom_read(input int addr, output int data);
 
+/* 异步读 */
+
 always @(*) begin
     irom_read({16'h8000, 2'b0000, a, 2'b00}, spo);
 end
+
+/* 同步读 (待支持) */
+/*
+always @(posedge clk) begin
+    irom_read({16'h8000, 2'b0000, a, 2'b00}, spo);
+end
+*/
+
 endmodule
