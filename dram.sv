6640c7b147d3e27e4908df74e5109b4e3df9e6aa
25050157 <1409887338@qq.com>
2026-05-12 11:18:48 +0800

    DRAM读出路径优化： 1.进行一拍寄存 2.把w/h/b相关处理放到lsu/keep_data之后

--- dram.sv
+++ dram.sv
@@ -10,7 +10,6 @@ import "DPI-C" function void dram_read(input int addr, output int data);
 import "DPI-C" function void dram_write(input int addr, input int data);
 
 /* 异步读，同步写 */
-/*
 always @(*) begin
     dram_read({12'h801, 2'b00, a, 2'b00}, spo);
 end
@@ -20,14 +19,25 @@ always @(posedge clk) begin
         dram_write({12'h801, 2'b00, a, 2'b00}, d);
     end
 end
-*/
 
+endmodule
+
+module blk_mem_gen_0(
+    input clka,
+    input [15:0] addra,
+    output [31:0] douta,
+    input wea,
+    input [31:0] dina
+); 
+
+import "DPI-C" function void dram_read(input int addr, output int data);
+import "DPI-C" function void dram_write(input int addr, input int data);
 /* 同步读，同步写 */
 
-always @(posedge clk) begin
-    dram_read({12'h801, 2'b00, a, 2'b00}, spo);
-    if (we) begin
-        dram_write({12'h801, 2'b00, a, 2'b00}, d);
+always @(posedge clka) begin
+    dram_read({12'h801, 2'b00, addra, 2'b00}, douta);
+    if (wea) begin
+        dram_write({12'h801, 2'b00, addra, 2'b00}, dina);
     end
 end
 
--- dram_driver.sv
+++ dram_driver.sv
@@ -22,6 +22,7 @@
 
 module dram_driver(
     input  logic         clk				,
+    input  logic         rst				,
 
     input  logic [17:0]  perip_addr			,
     input  logic [31:0]  perip_wdata		,
@@ -30,13 +31,22 @@ module dram_driver(
     output logic [31:0]  perip_rdata		
 );
     logic [15:0] dram_addr;
-    logic [ 1:0] offset;
+    wire [ 1:0] offset;
     logic [31:0] dram_data, dram_rdata_raw, dout;
+    reg [31:0] reg_dram_rdata_raw;
 
-    assign dram_addr = perip_addr[17:2];
+    reg [15:0] reg_addr;
+    reg [1:0] reg_mask;
+    reg [1:0] reg_offset;
+    reg [31:0] reg_wdata;
+    reg [31:0] reg_rdata_raw;
+    reg reg_wen;
+
     assign offset = perip_addr[1:0];
     assign perip_rdata = dout;
 
+
+    /*
     DRAM Mem_DRAM (
         .clk        (clk),
         .a          (dram_addr),
@@ -44,47 +54,75 @@ module dram_driver(
         .we         (dram_wen),
         .d          (dram_data)
     );
+    */
 
-    // dram_rdata_raw process, lh lb
+    blk_mem_gen_0 Mem_blk_DRAM (
+        .clka        (clk),
+        .addra          (dram_addr),
+        .douta        (dram_rdata_raw),
+        .wea         (reg_wen),
+        .dina          (dram_data)
+    );
+
+    /* 正常读 */
     always_comb begin
-        dout = 0;
-        case (perip_mask)
-            2'b00: // lb/lbu
-                case (offset)
-                    2'b00:  dout = {24'b0, dram_rdata_raw[7:0]};
-                    2'b01:  dout = {24'b0, dram_rdata_raw[15:8]};
-                    2'b10:  dout = {24'b0, dram_rdata_raw[23:16]};
-                    2'b11:  dout = {24'b0, dram_rdata_raw[31:24]};
-                endcase
-            2'b01: // lh/lhu
-                case (offset[1])
-                    1'b0:  dout = {16'b0, dram_rdata_raw[15:0]};
-                    1'b1:  dout = {16'b0, dram_rdata_raw[31:16]};
-                endcase
-            2'b10: dout = dram_rdata_raw;
-            default: dout = 0;
-        endcase
+        dout = reg_dram_rdata_raw;
+    end
+    always @(posedge clk) begin
+        if (rst) begin
+            reg_dram_rdata_raw <= 0;
+        end
+        else begin
+            reg_dram_rdata_raw <= dram_rdata_raw;
+        end
     end
 
-    // dram_data_raw process, sh, sb
+    /* 延迟1周期写 */
+
+    assign dram_addr = reg_wen ? reg_addr : perip_addr[17:2] ;
+    
+    //寄存信息
+    always @(posedge clk) begin
+        if (rst) begin
+            reg_addr <= 0;
+            reg_mask <= 0;
+            reg_offset <= 0;
+            reg_wdata <= 0;
+            reg_rdata_raw <= 0;
+            reg_wen <= 0;
+        end
+        else if (dram_wen) begin
+            reg_addr <= perip_addr[17:2];
+            reg_mask <= perip_mask;
+            reg_offset <= perip_addr[1:0];
+            reg_wdata <= perip_wdata;
+            reg_rdata_raw <= dram_rdata_raw;
+            reg_wen <= 1;
+        end
+        else begin
+            reg_wen <= 0;
+        end
+    end
+
+    // dram_data_raw process, sh, sb(全部使用寄存过的信号)
     always_comb begin
-        case (perip_mask)
-            2'b10: dram_data = perip_wdata;  // sw
+        case (reg_mask)
+            2'b10: dram_data = reg_wdata;  // sw
             2'b01: begin           // sh
-                case (offset[1])
-                    1'b0: dram_data = {dram_rdata_raw[31:16], perip_wdata[15:0]};
-                    1'b1: dram_data = {perip_wdata[15:0], dram_rdata_raw[15:0]};
+                case (reg_offset[1])
+                    1'b0: dram_data = {reg_rdata_raw[31:16], reg_wdata[15:0]};
+                    1'b1: dram_data = {reg_wdata[15:0], reg_rdata_raw[15:0]};
                 endcase
             end
             2'b00: begin           // sb
-                case (offset)
-                    2'b00: dram_data = {dram_rdata_raw[31:8], perip_wdata[7:0]};
-                    2'b01: dram_data = {dram_rdata_raw[31:16], perip_wdata[7:0], dram_rdata_raw[7:0]};
-                    2'b10: dram_data = {dram_rdata_raw[31:24], perip_wdata[7:0], dram_rdata_raw[15:0]};
-                    2'b11: dram_data = {perip_wdata[7:0], dram_rdata_raw[23:0]};
+                case (reg_offset)
+                    2'b00: dram_data = {reg_rdata_raw[31:8], reg_wdata[7:0]};
+                    2'b01: dram_data = {reg_rdata_raw[31:16], reg_wdata[7:0], reg_rdata_raw[7:0]};
+                    2'b10: dram_data = {reg_rdata_raw[31:24], reg_wdata[7:0], reg_rdata_raw[15:0]};
+                    2'b11: dram_data = {reg_wdata[7:0], reg_rdata_raw[23:0]};
                 endcase
             end
-            default: dram_data = perip_wdata;
+            default: dram_data = reg_wdata;
         endcase
     end
 endmodule
